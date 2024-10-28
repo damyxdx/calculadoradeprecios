7db1283666e957897cc5b3d6e37fa6d9f4612747
A Damian Garcia <65087982+damyxdx@users.noreply.github.com>
2024-10-28 09:25:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function calculate() {
     const brand = document.getElementById('brand').value;
     const inputValue = parseFloat(document.getElementById('inputValue').value);
@@ -11,10 +10,10 @@ function calculate() {
 
     switch (brand) {
         case 'General':
-            result = inputValue * 1.21 * 2 +500;
+            result = inputValue * 1.21 * 2 + 500;
             break;
-        case 'Fashion/bathbazar/':
-            result = inputValue * 1.26 * 2 +500;
+        case 'Fashion/Bathbazar':
+            result = inputValue * 1.26 * 2 + 500;
             break;
         case 'marca3':
             result = inputValue * 1.21 * 2;
@@ -23,13 +22,32 @@ function calculate() {
             result = inputValue * 1.21 * 2;
     }
 
-    // Ajustar el resultado para que termine en "50" o "00"
     const remainder = result % 100;
     if (remainder < 50) {
-        result = result - remainder + 50; // Redondear hacia abajo a "50"
+        result = result - remainder + 50;
     } else {
-        result = result - remainder + 100; // Redondear hacia arriba a "00"
+        result = result - remainder + 100;
     }
 
     document.getElementById('result').textContent = result.toFixed(2);
+    addToHistory(brand, inputValue, result.toFixed(2));
+}
+
+function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+        calculate();
+    }
+}
+
+function addToHistory(brand, inputValue, result) {
+    const historyTable = document.getElementById('historyTable').getElementsByTagName('tbody')[0];
+    const newRow = historyTable.insertRow();
+
+    const brandCell = newRow.insertCell(0);
+    const inputValueCell = newRow.insertCell(1);
+    const resultCell = newRow.insertCell(2);
+
+    brandCell.textContent = brand;
+    inputValueCell.textContent = inputValue;
+    resultCell.textContent = result;
 }
